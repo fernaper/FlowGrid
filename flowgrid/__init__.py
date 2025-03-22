c9c6b7bf59e48cdd554762f314a5bf86725027a1
Fernando <fernaperg@gmail.com>
2025-03-22 18:22:39 +0100

    :tada: v0.2.0 Now it can be launched as a module

--- flowgrid/__init__.py
+++ flowgrid/__init__.py
@@ -2,4 +2,4 @@ from .base import FlowGrid, Task
 from .celery_app import make_celery
 
 __all__ = ['FlowGrid', 'Task', 'make_celery', '__version__']
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
         requirements = f.read().split('\n')
 
     setup(
-        name='flowgrid',
+        name='py-flowgrid',
         version=__version__,
         license='MIT',
-        description='Flowgrid is a simple task queue for Python',
+        description='Flowgrid is a simple and powerful task queue for Python',
         long_description=long_description,
         long_description_content_type='text/markdown',
         author='Fernando Pérez',
